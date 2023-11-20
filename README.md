439faac2c69bc4e08db9e049bfc77329622aeeaa
javedmughal <javedmughal609@gmail.com>
2023-11-20 10:17:07 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# table_calender_in_flutter
+ Calender related working app
---
+++ android/app/src/main/kotlin/com/example/table_calender_in_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.table_calender_in_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
